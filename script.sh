3e0d5fe603a30ab0ef2904895208eddb2d308263
CraigM5432 <116079017+CraigM5432@users.noreply.github.com>
2026-01-29 20:23:45 +0000

    Delete folders on script start

--- script.sh
+++ script.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
-mkdir -p fol_1 fol_2
+# Remove folders if they already exist
+rm -rf fol_1 fol_2
 
+# Recreate folders
+mkdir fol_1 fol_2
+
+# Create files in fol_1
 touch fol_1/1_1.txt fol_1/1_2.txt fol_1/1_3.txt
+
+# Create files in fol_2
 touch fol_2/2_1.txt fol_2/2_2.txt fol_2/2_3.txt
 
 echo "Job completed"
+
